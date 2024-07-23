7da494cbd73969ebeec19ea1e04fd60eec17b7ce
praveen2903 <vvit20bq1a05l0@gmail.com>
2024-07-23 13:19:14 +0530

    faqs updated

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,33 +1,33 @@
 const questions = [
     {
       id: 1,
-      title: "Who can open a Draft account?",
+      title: "Is my chat data secure?",
       description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni voluptate unde vero corporis tempora praesentium laboriosam ratione incidunt a odit.",
+        "Yes, your chat data is secure. Our app uses end-to-end encryption to ensure that only you and the person you're communicating with can read the messages. Not even the app developers have access to your messages.",
     },
     {
       id: 2,
-      title: "What if I withdraw money abroad?",
+      title: "How do I customize my chat app appearance?",
       description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni voluptate unde vero corporis tempora praesentium laboriosam ratione incidunt a odit.",
+        "To customize the appearance, go to the app settings and select Appearance or Theme. You can choose from different themes, change the chat background, and adjust other visual settings.",
     },
     {
       id: 3,
-      title: "What happens when my subscription ends?",
+      title: "How do I log out of the chat app?",
       description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni voluptate unde vero corporis tempora praesentium laboriosam ratione incidunt a odit.",
+        "To log out, go to the app settings and select Log Out. Confirm your action, and you will be logged out of your account.",
     },
     {
       id: 4,
-      title: "Is this service better than YouBay?",
+      title: "How do I report a problem or provide feedback?",
       description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni voluptate unde vero corporis tempora praesentium laboriosam ratione incidunt a odit.",
+        "To report a problem or provide feedback, go to the app settings, and select Help & Feedback or Support. You can then describe the issue you're facing or provide suggestions for improvement. You may also find a Contact Us option to reach the support team directly.",
     },
     {
       id: 5,
-      title: "What happens if I withdraw more than $250?",
+      title: "How do I send images or files?",
       description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni voluptate unde vero corporis tempora praesentium laboriosam ratione incidunt a odit.",
+        "In a chat window, click on the attachment icon (usually a paperclip or camera icon). You can then choose to send images or files from your device. Select the files you want to send and click Send.",
     },
   ];
   
